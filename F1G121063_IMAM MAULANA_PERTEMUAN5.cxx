3a060232411adc64547223a6cf34d63107b18dd7
Imam20 <92580982+Imam20@users.noreply.github.com>
2021-11-11 21:15:56 +0800

    Add files via upload

---
+++ F1G121063_IMAM MAULANA_PERTEMUAN5.cxx
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int j,b;
+	cout<<"masukan bilangan mulia awal  :  ";
+	cin>>j;
+	cout<<"ingin sampai berapa  :  ";
+	cin>>b;
+	do {
+		cout<<j<<"  x  "<<j<<" = "<<j*j<<endl;
+		j++;
+		}
+	while(j<=b);
+	
+	
+	}
